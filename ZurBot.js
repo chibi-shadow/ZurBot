00154f9d8ce3235bd278681a91e12467fc4a5766
zurbo <zurbo00@gmail.com>
2015-03-11 15:27:29 +0100

    update ZurBot.js

--- ZurBot.js
+++ ZurBot.js
@@ -2,10 +2,16 @@ init()
 
 function init() {
 	API.setVolume(0);
-	$("#chat-emoji-button").click();
-	$("#chat-sound-button").click();
-	$().click();
-	$().click();
+	if (document.querySelector('class.icon.icon-emoji-on')!==null){
+		$("#chat-emoji-button").click();
+	}
+	if (document.querySelector('class.icon.icon-chat-sound-on')!==null){
+		$("#chat-sound-button").click();
+	}
+	document.querySelector('div.item.s-av.selected').click();
+	if (document.querySelector('div.item.s-vo.selected')!==null){
+		document.querySelector('div.item.s-vo').click();
+	}
 	API.sendChat(endInit);
 }
 

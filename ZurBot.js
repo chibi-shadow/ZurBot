135122876d0766b389aa66c39d5bb053654d5a14
zurbo <zurbo00@gmail.com>
2015-03-13 06:33:34 +0100

    update ZurBot.js (add the printchat fct)

--- ZurBot.js
+++ ZurBot.js
@@ -1,19 +1,27 @@
 function init() {
 	document.getElementById("playback").removeChild(document.getElementById("playback-container"));
-	if (document.querySelector("#chat-emoji-button .icon-emoji-on")){
+	if (document.querySelector("#chat-emoji-button .icon-emoji-on")) {
 		$("#chat-emoji-button").click();
 	}
 	if (document.querySelector("#chat-sound-button .icon-chat-sound-on")) {
 		$("#chat-sound-button").click();
 	}
-	API.sendChat("/cap 1")
+	API.sendChat("/cap 1");
 	// affichage fin init
 }
 
-var nomEvenement = new CustomEvent( 
-	"Bot.chatCommand",
-	{
-		detail: {
-			
+function printChat(txt) { // a corriger 
+	for (var i = 0; i < arguments.lenght; i++) {
+		txt = txt.split("@" + (i));
+		txt[2] = txt[1];
+		txt[1] = arguments[i + 1];
+		txt = txt.join('');
+	}
+	API.sendChat(txt);
+}
 
-init()
+/*var BOTAPI.CHATCOMMAND = new CustomEvent(
+		"Bot.chatCommand", {
+		detail : {
+
+			init()*/
